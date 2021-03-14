9816f5f7ba4b880eb2268ae1f5312000558e0d1b
Joseph Lozano <joseph@hey.com>
2021-03-14 19:44:46 -0400

    fix dialyzer

--- lib/mix/tasks/build.ex
+++ lib/mix/tasks/build.ex
@@ -2,12 +2,17 @@ defmodule Mix.Tasks.Blog.Build do
   use Mix.Task
   import EExHTML
 
+  @spec run(any) :: any()
   def run(_args) do
     Mix.shell().info("Generating Static Files to ./site")
-    name = "Blog Generator"
+
+    name = "World"
     content = ~E"<h1>Hello <%= name %></h1>"
+    html = to_string(content)
+
     File.mkdir("./_site")
-    File.write("./_site/index.html", to_string(content), [:write, :ut8])
-    Mix.shell().info("Done Generating Static Files")
+    File.write("./_site/index.html", html, [:write, :utf8])
+
+    Mix.shell().info("Done")
   end
 end
