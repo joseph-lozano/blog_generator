70eeccdbbcf47f8911d979a36504cb9f9927a706
Joseph Lozano <joseph@hey.com>
2021-03-14 21:02:52 -0400

    index page

--- lib/mix/tasks/build.ex
+++ lib/mix/tasks/build.ex
@@ -5,22 +5,27 @@ defmodule Mix.Tasks.Blog.Build do
   def run(_args) do
     Mix.shell().info("Generating Static Files to ./site")
 
-    content = EEx.eval_file("site/index.html.eex", name: "File")
-
     {:ok, posts} = File.ls("site/posts")
 
     File.mkdir("./_site")
+
+    posts =
+      Enum.map(posts, fn post ->
+        title = Path.rootname(post)
+        inner_content = Earmark.as_html!(File.read!("site/posts/#{post}"))
+
+        Mix.shell().info("Writing #{title}.html")
+
+        content = EEx.eval_file("site/post.html.eex", inner_content: inner_content)
+        File.write("./_site/#{title}.html", content, [:write, :utf8])
+
+        title
+      end)
+
+    Mix.shell().info("Writing index.html")
+
+    content = EEx.eval_file("site/index.html.eex", posts: posts)
     File.write("./_site/index.html", content, [:write, :utf8])
-
-    Enum.each(posts, fn post ->
-      title = Path.rootname(post)
-      inner_content = Earmark.as_html!(File.read!("site/posts/#{post}"))
-
-      content = EEx.eval_file("site/post.html.eex", inner_content: inner_content)
-
-      Mix.shell().info("Writing #{title}")
-      File.write("./_site/#{title}.html", content, [:write, :utf8])
-    end)
 
     Mix.shell().info("Done")
   end
