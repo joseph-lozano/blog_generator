e1fe3bec0d893afd829669ca0ffd59b07ffc322c
Joseph Lozano <joseph@hey.com>
2021-03-14 21:59:32 -0400

    refactor build task

--- lib/mix/tasks/build.ex
+++ lib/mix/tasks/build.ex
@@ -1,32 +1,55 @@
 defmodule Mix.Tasks.Blog.Build do
   use Mix.Task
 
+  @source_dir "site"
+  @dest_dir "_site"
+
   @spec run(any) :: any()
   def run(_args) do
-    Mix.shell().info("Generating Static Files to ./site")
+    make_directory()
 
-    {:ok, posts} = File.ls("site/posts")
+    posts = make_posts()
 
-    File.mkdir("./_site")
+    make_index(posts)
+  end
 
-    posts =
-      Enum.map(posts, fn post ->
-        title = Path.rootname(post)
-        inner_content = Earmark.as_html!(File.read!("site/posts/#{post}"))
+  defp make_directory() do
+    File.mkdir(@dest_dir)
+  end
 
-        Mix.shell().info("Writing #{title}.html")
+  defp make_posts() do
+    get_posts()
+    |> raise_if_duplicates()
+    |> save_posts()
+  end
 
-        content = EEx.eval_file("site/post.html.eex", inner_content: inner_content)
-        File.write("./_site/#{title}.html", content, [:write, :utf8])
+  def get_posts() do
+    File.ls!("#{@source_dir}/posts")
+  end
 
-        title
-      end)
+  def raise_if_duplicates(posts) do
+    if length(posts) == length(Enum.uniq(posts)) do
+      posts
+    else
+      raise "Duplicate filenames detected. Aborting."
+      System.halt(1)
+    end
+  end
 
-    Mix.shell().info("Writing index.html")
+  defp save_posts(posts) do
+    Enum.map(posts, fn post ->
+      title = Path.rootname(post)
+      inner_content = Earmark.as_html!(File.read!("#{@source_dir}/posts/#{post}"))
 
-    content = EEx.eval_file("site/index.html.eex", posts: posts)
-    File.write("./_site/index.html", content, [:write, :utf8])
+      content = EEx.eval_file("#{@source_dir}/post.html.eex", inner_content: inner_content)
+      File.write("#{@dest_dir}/#{title}.html", content, [:write, :utf8])
 
-    Mix.shell().info("Done")
+      title
+    end)
+  end
+
+  defp make_index(posts) do
+    content = EEx.eval_file("#{@source_dir}/index.html.eex", posts: posts)
+    File.write("#{@dest_dir}/index.html", content, [:write, :utf8])
   end
 end
