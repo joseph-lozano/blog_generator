ba9ab4be6e55ab3d55e338b002cc7d995d221692
Joseph Lozano <joseph@hey.com>
2021-04-06 12:49:49 -0400

    What I'm doing now

--- lib/blog.ex
+++ lib/blog.ex
@@ -27,6 +27,25 @@ defmodule Blog do
     copy_resume()
     copy_css()
     copy_favicon()
+    copy_markdown()
+  end
+
+  def copy_markdown() do
+    Path.wildcard("#{@source_dir}/*.md")
+    |> Enum.each(fn file ->
+      [yaml, inner_content] = file |> File.read!() |> String.split("---", parts: 2, trim: true)
+      attrs = YamlElixir.read_from_string!(yaml)
+      %{"title" => title, "slug" => slug} = attrs
+
+      content =
+        EEx.eval_file("#{@source_dir}/post.html.eex",
+          post: %{title: title, description: nil, draft: false},
+          inner_content: inner_content,
+          render: &EEx.eval_file/2
+        )
+
+      File.write("#{@dest_dir}/#{slug}.html", content, [:write])
+    end)
   end
 
   defp copy_resume() do
@@ -118,7 +137,11 @@ defmodule Blog do
       |> Blog.Highlighter.highlight()
 
     content =
-      EEx.eval_file("#{@source_dir}/post.html.eex", post: post, inner_content: inner_content)
+      EEx.eval_file("#{@source_dir}/post.html.eex",
+        post: post,
+        inner_content: inner_content,
+        render: &EEx.eval_file/2
+      )
 
     File.write("#{@dest_dir}/#{post.slug}.html", content, [:write])
 
@@ -135,7 +158,11 @@ defmodule Blog do
       |> Enum.sort_by(& &1.date, {:desc, Date})
       |> Enum.filter(&show_draft?/1)
 
-    content = EEx.eval_file("#{@source_dir}/index.html.eex", posts: posts)
+    content =
+      EEx.eval_file("#{@source_dir}/index.html.eex",
+        posts: posts,
+        render: &EEx.eval_file/2
+      )
 
     File.write("#{@dest_dir}/index.html", content, [:write])
   end
--- lib/blog/watcher.ex
+++ lib/blog/watcher.ex
@@ -14,17 +14,13 @@ if Mix.env() != :prod do
     end
 
     def handle_continue(:subscribe, _state) do
-      {:ok, pid} = FileSystem.start_link(dirs: ["site/"])
+      {:ok, pid} = FileSystem.start_link(dirs: ["site/", "lib/"])
       FileSystem.subscribe(pid)
       {:noreply, pid}
     end
 
-    def handle_info({:file_event, _, {file, _}}, state) do
-      cond do
-        String.ends_with?(file, "post.html.eex") -> Blog.make_posts()
-        String.ends_with?(file, "index.html.eex") -> Blog.make_index()
-        true -> Blog.make_post(file)
-      end
+    def handle_info({:file_event, _, {_file, _}}, state) do
+      Blog.make()
 
       {:noreply, state}
     end
